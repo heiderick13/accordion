37fecd59bd7d692cb791bea29c617dcc2d33a5cf
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-05-19 11:44:37 -0300

    update accordion component

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,5 +1,14 @@
+import data from "../../data.json";
+import AccordionItem from "../AccordionItem/AccordionItem";
+
 function Accordion() {
-  return <div>Accordion</div>;
+  return (
+    <div>
+      {data.map((item, index) => (
+        <AccordionItem item={item} key={index} />
+      ))}
+    </div>
+  );
 }
 
 export default Accordion;
