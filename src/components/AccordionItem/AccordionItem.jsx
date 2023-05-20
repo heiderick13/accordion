be82380b9deb7b064ccb0262747f80b9343bfe27
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-05-20 10:40:48 -0300

    :lipstick: update acoordionItem styles

--- src/components/AccordionItem/AccordionItem.jsx
+++ src/components/AccordionItem/AccordionItem.jsx
@@ -1,7 +1,11 @@
+import { BsChevronDown } from "react-icons/bs";
+
 function AccordionItem({ item }) {
   return (
-    <div className="">
-      <h2>{item.question}</h2>
+    <div className="text-black">
+      <h2 className="flex items-center justify-between gap-3 text-2xl">
+        {item.question} <BsChevronDown className="text-red-400" />
+      </h2>
       <p className="hidden">{item.answer}</p>
     </div>
   );
