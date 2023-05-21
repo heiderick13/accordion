b2b0e6ee21038317e1259c11a9f280bae465fa88
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-05-21 14:54:57 -0300

    :lipstick: position images

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import Accordion from "./components/Accordion/Accordion";
 
+import desktopBox from "./assets/images/illustration-box-desktop.svg";
+import desktopWoman from "./assets/images/illustration-woman-online-desktop.svg";
+
 function App() {
   return (
-    <>
-      <div>
-        <h1>APP</h1>
+    <div className="h-screen flex items-center justify-center">
+      <div className="main-container relative bg-white rounded-3xl flex justify-between items-center gap-6 pe-10 py-14">
+        <div className="img-container h-full w-1/2 overflow-hidden ">
+          <img
+            className="absolute translate-y-32 -translate-x-24  z-20"
+            src={desktopBox}
+            alt=""
+          />
+          <img className=" -translate-x-16 z-10" src={desktopWoman} alt="" />
+        </div>
         <Accordion />
       </div>
-    </>
+    </div>
   );
 }
 
