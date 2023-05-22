dd2fc943e45e51d4c3b3a11ddad354a5c1591a66
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-05-22 18:20:02 -0300

    add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      "verydark-desaturated-blue": "hsl(238, 29%, 16%)",
+      "soft-red": "hsl(14, 88%, 65%)",
+      "soft-violet": "hsl(273, 75%, 66%)",
+      "soft-blue": "hsl(240, 73%, 65%)",
+      "verydark-grayish-blue": "hsl(237, 12%, 33%)",
+      "dark-grayish-blue": "hsl(240, 6%, 50%)",
+      "light-grayish-blue": "hsl(240, 5%, 91%)",
+      white: "hsl(0, 0%, 100%)",
+    },
     extend: {},
   },
   plugins: [],
