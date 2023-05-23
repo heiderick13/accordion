cc94f999dd9065865fbc8dea55ade8fb7b49fa1c
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-05-23 09:56:43 -0300

    :lipstick: update text color

--- src/components/AccordionItem/AccordionItem.jsx
+++ src/components/AccordionItem/AccordionItem.jsx
@@ -4,7 +4,7 @@ function AccordionItem({ item }) {
   return (
     <div className="text-black">
       <h2 className="flex items-center justify-between gap-3 text-2xl">
-        {item.question} <BsChevronDown className="text-red-400" />
+        {item.question} <BsChevronDown className="text-soft-red" />
       </h2>
       <p className="hidden">{item.answer}</p>
     </div>
