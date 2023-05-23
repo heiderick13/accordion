011122d5eab17b5c96e3dd8ab05192cd32a22f64
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-05-23 12:15:07 -0300

    :sparkles: toggle answer

--- src/components/AccordionItem/AccordionItem.jsx
+++ src/components/AccordionItem/AccordionItem.jsx
@@ -1,12 +1,25 @@
-import { BsChevronDown } from "react-icons/bs";
+import { BsChevronDown, BsChevronUp } from "react-icons/bs";
+
+import { useState } from "react";
 
 function AccordionItem({ item }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const open = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="text-black">
+    <div className="select-none cursor-pointer" onClick={open}>
       <h2 className="flex items-center justify-between gap-3 text-2xl">
-        {item.question} <BsChevronDown className="text-soft-red" />
+        {item.question}{" "}
+        <BsChevronDown
+          className={!isOpen ? "text-soft-red" : "text-soft-red rotate-180"}
+        />
       </h2>
-      <p className="hidden">{item.answer}</p>
+      <p className={`w-[80%] + ${!isOpen ? "hidden" : "block"}`}>
+        {item.answer}
+      </p>
     </div>
   );
 }
