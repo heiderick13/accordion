f1f37d79c08a0d5761620898ba3d17198ebb6d16
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-05-24 18:13:20 -0300

    :lipstick: set min-h / hide scrollbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
 import Accordion from "./components/Accordion/Accordion";
 
-import desktopBox from "./assets/images/illustration-box-desktop.svg";
-import desktopWoman from "./assets/images/illustration-woman-online-desktop.svg";
+import woman from "./assets/images/illustration-woman-online-mobile.svg";
 
 function App() {
   return (
-    <div className="h-screen flex items-center justify-center bg-gradient-to-b from-soft-violet to-soft-blue text-verydark-desaturated-blue">
-      <div className="main-container relative bg-white rounded-3xl flex justify-between items-center gap-6 pe-10 py-14">
-        <div className="img-container h-full w-1/2 overflow-hidden ">
-          <img
-            className="absolute translate-y-32 -translate-x-24  z-20"
-            src={desktopBox}
-            alt=""
-          />
-          <img className=" -translate-x-16 z-10" src={desktopWoman} alt="" />
+    <div className="min-h-screen p-5 flex items-center justify-center bg-gradient-to-b from-soft-violet to-soft-blue text-verydark-desaturated-blue">
+      <div className="main-container max-w-[35%] bg-white rounded-3xl flex flex-col justify-center items-center gap-6 px-10 py-14">
+        <img className="h-52" src={woman} alt="" />
+        <div className="">
+          <h1 className="text-2xl text-center font-bold pb-6">FAQ</h1>
+          <Accordion />
         </div>
-        <Accordion />
       </div>
     </div>
   );
